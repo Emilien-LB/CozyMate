a156d3a9ddfb50526e9bcc04ce2be08b1d16c709
Anais Bernard <anais.mauricette.bernard@gmail.com>
2023-09-04 20:35:35 +0200

    event dependant

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :scheduled_tasks
   has_many :tasks, through: :scheduled_tasks
-  has_many :events
+  has_many :events, dependent: :destroy
 
   validates :first_name, :last_name, :birth_date, presence: true
   # validates :phone_number, format: { with: /\A(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}\z/ }
